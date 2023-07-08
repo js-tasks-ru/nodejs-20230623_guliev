92e4fa094e58c158c304542252e7ee27c1a882e8
anar <guliev@internet.ru>
2023-07-08 22:58:42 +0300

    Finished 01, 02

--- 03-streams/02-line-split-stream/LineSplitStream.js
+++ 03-streams/02-line-split-stream/LineSplitStream.js
@@ -1,15 +1,30 @@
 const stream = require('stream');
-const os = require('os');
 
 class LineSplitStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this.remainingData = '';
   }
 
   _transform(chunk, encoding, callback) {
+    const data = this.remainingData + chunk.toString();
+    const lines = data.split(/\r?\n/);
+
+    for (let i = 0; i < lines.length - 1; i++) {
+      this.push(lines[i]);
+    }
+
+    this.remainingData = lines[lines.length - 1];
+
+    callback();
   }
 
   _flush(callback) {
+    if (this.remainingData) {
+      this.push(this.remainingData);
+    }
+
+    callback();
   }
 }
 
