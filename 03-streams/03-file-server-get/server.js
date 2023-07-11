8d88b66dce9be39ac8a46a4cc4af5f11f04da37e
anar <guliev@internet.ru>
2023-07-11 22:56:42 +0300

    finished 03

--- 03-streams/03-file-server-get/server.js
+++ 03-streams/03-file-server-get/server.js
@@ -1,6 +1,7 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 const server = new http.Server();
 
 server.on('request', (req, res) => {
@@ -11,9 +12,23 @@ server.on('request', (req, res) => {
 
   switch (req.method) {
     case 'GET':
+      const isNested = pathname.includes('/');
 
+      if(isNested){
+        res.statusCode = 400;
+        res.end();
+      } else {
+        fs.readFile(filepath, (err, data) => {
+          if (err) {
+            res.statusCode = 404;
+            res.end();
+          } else {
+            res.statusCode = 200;
+            res.end(data);
+          }
+        });
+      }
       break;
-
     default:
       res.statusCode = 501;
       res.end('Not implemented');
